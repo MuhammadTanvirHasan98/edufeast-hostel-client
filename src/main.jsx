3547f1a70cdfd7efa27ed1b241519780e6304f7d
MuhammadTanvirHasan98 <muhammadtanvirhasan98@gmail.com>
2024-06-17 22:39:12 +0600

    Added AuthProvider component successfully.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from './Routes/Router';
-import {
-  QueryClient,
-  QueryClientProvider
-} from '@tanstack/react-query'
+import router from "./Routes/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./Authentication/Provider/AuthProvider";
 
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+  <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </AuthProvider>
   // </React.StrictMode>,
-)
+);
